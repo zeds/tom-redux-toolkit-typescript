82212c8daf36de533e30e095717581799821b610
Tsutomu Okumura <tom.zed39@gmail.com>
2022-12-28 17:58:07 +0900

    取り消し直前の修正

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   return (
+			<>
         <h1 className="text-3xl font-bold underline">
             Hello world!
         </h1>
+				<p>直前の修正</p>
+			</>
   )
 }
 
