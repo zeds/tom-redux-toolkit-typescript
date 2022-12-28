0514488771fd48be599ec34fcd6a7c3fc8aadf48
Tsutomu Okumura <tom.zed39@gmail.com>
2022-12-29 01:13:33 +0900

    レイアウト調整

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -8,16 +8,18 @@ export function Counter() {
   const dispatch = useDispatch()
 
   return (
-    <div>
-      <div>
+    <div className="bg-gray-300 w-full h-screen p-4">
+      <div className="flex">
         <button
+					className="bg-blue-300 p-3 rounded mx-2"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           Increment
         </button>
-        <span>{count}</span>
+        <div className="bg-yellow-300 p-4 w-[100px] text-center">{count}</div>
         <button
+					className="bg-blue-300 p-3 rounded mx-2"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
