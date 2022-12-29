0c8759ac496e0fe52dfad6a974f942ecd93741d7
Tsutomu Okumura <tom.zed39@gmail.com>
2022-12-30 00:42:17 +0900

    merge modal

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,11 @@ import { TodoContainer } from './features/todos/components/TodoContainer';
 const App: FC = () => {
 	const { isOpen } = useSelector((store) => store.modal);
   return (
-<<<<<<< HEAD
-			<>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-				<p>直前の修正</p>
-			</>
-  )
-}
-=======
     <div className="modal-container bg-red-500">
       <TodoContainer />
 			{isOpen && <Modal />}
     </div>
   );
 };
->>>>>>> modal
 
 export default App;
